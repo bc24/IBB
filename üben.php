ab13ed60cdc2e4806c265a432967d219ecc6c62f
bc24 <frank@bc24.org>
2016-03-12 07:26:45 +0100

    Erste übungen

---
+++ üben.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Üben</title>
+	<style>
+	
+	h1{
+		font-size= "30px";
+	}
+	
+	
+	</style>
+	</head>
+<body>
+
+<h1>test</h1>
+
+</body>	
+	
+	
+
+	
+	
+	
+		</html>
