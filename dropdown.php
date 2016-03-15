664c6f7eec10f2bf748006549f968a6b34228618
Frank Panzer <frank@bc24.org>
2016-03-15 21:10:33 +0100

    dropdown verschoben

--- dropdown.php
+++
@@ -1,1 +0,0 @@
-<form> <select name="top5" size="5" multiple> <option>Heino</option> <option>Michael Jackson</option> <option>Tom Waits</option> <option>Nina Hagen</option> <option>Marianne Rosenberg</option> </select> </form>
