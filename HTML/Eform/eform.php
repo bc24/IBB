c9e39c0f37245d7250e3ac4521f679bf9b0dd5f0
Frank Panzer <frank@bc24.org>
2016-03-23 13:40:40 +0100

    Eingabeformular

---
+++ HTML/Eform/eform.php
@@ -0,0 +1,2 @@
+Hallo <?php echo $_POST['name']; ?>.
+Sie sind <?php echo $_POST['alter']; ?> Jahre alt.
---
+++ HTML/eform.php
@@ -0,0 +1,16 @@
+<!DOCTYPE! html>
+<html>
+	<head>
+		<title>Eingabeformular</title>
+	</head>
+<body>
+	<form action = "eform.phtml">
+	<input name="text">
+	<input type=submit value = "OK">
+	</form>
+<?php
+ echo ("<br> $text");
+ ?>
+</body>
+</html>
+	
